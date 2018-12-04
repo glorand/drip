8f783490c652185735b9d9eeb567552e3834edde
Gombos Lorand <gombos.lorand@gmail.com>
2018-12-04 16:45:43 +0200

    Fix

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,7 +4,7 @@ namespace Glorand\Drip\Tests;
 
 class TestCase extends \PHPUnit\Framework\TestCase
 {
-    protected $apiToken = '8e09e96326ad1b06e68cb0c562df2d07';
-    protected $accountId = '1794815';
+    protected $apiToken = '123890065343';
+    protected $accountId = '187345';
     protected $userAgent = 'Test User Agent';
 }
